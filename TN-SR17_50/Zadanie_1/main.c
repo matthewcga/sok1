15b9b5d699ef9d2fdca912fcb095aaa4982649a7
matthewcga <mateusz.cga@gmail.com>
2022-04-20 18:24:15 +0200

    2

--- TN-SR17_50/Zadanie_1/main.c
+++ TN-SR17_50/Zadanie_1/main.c
@@ -15,42 +15,54 @@
  * Jeśli plik nie istnieje, należy go stworzyć w taki sposób, by był możliwy
  * do zapisu i odczytu przez właściciela i roota, ale nie przez innych użytkowników.
  */
-int open_file(const char* path) {
+int open_file(const char *path)
+{
+    int fd = open(path, O_APPEND);
 
+    if (fd == -1)
+        fd = open(path, S_IRWXU);
+
+    return fd;
 }
 
-void check_fd(int fd) {
-    if (fd == -1) {
+void check_fd(int fd)
+{
+    if (fd == -1)
+    {
         perror("Failed to open file");
         exit(-1);
     }
     char buf[1];
     int r = read(fd, buf, 1);
-    if (r != -1 || errno != EBADF) {
+    if (r != -1 || errno != EBADF)
+    {
         fprintf(stderr, "Error, can read\n");
         exit(-1);
     }
 }
 
-int main(void) {
+int main(void)
+{
     unlink(FILE_NAME);
     int fd1 = open_file(FILE_NAME);
     int fd2 = open_file(FILE_NAME);
     check_fd(fd1);
     check_fd(fd2);
     char buf[SIZE] = {};
-    
+
     write(fd1, buf, SIZE);
     lseek(fd1, 0, SEEK_SET);
     write(fd1, buf, SIZE);
     off_t pos = lseek(fd1, 0, SEEK_CUR);
-    if (pos != 2 * SIZE) {
+    if (pos != 2 * SIZE)
+    {
         fprintf(stderr, "Error, non-appending writes are possible\n");
         exit(-1);
     }
     struct stat s;
     fstat(fd1, &s);
-    if (s.st_mode & 0777 != 0600) {
+    if (s.st_mode & 0777 != 0600)
+    {
         fprintf(stderr, "Error, wrong permissions: %o\n", s.st_mode & 0777);
         exit(-1);
     }
--- TN-SR17_50/Zadanie_2/main.c
+++ TN-SR17_50/Zadanie_2/main.c
@@ -4,7 +4,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
 /*
  * Funkcja 'read_end' powinna:
  *  - otworzyc plik o nazwie przekazanej w argumencie
@@ -12,18 +11,22 @@
  *  - przeczytac ostatnie 8 bajtow tego pliku i zapisac
  *    wynik w argumencie 'result'.
  */
-void read_end(char *file_name, char *result){
-    // Uzupelnij cialo funkcji read_end zgodnie z
-    // komentarzem powyzej
 
+void read_end(char *file_name, char *result)
+{
+    int fd = open(file_name, O_RDONLY);
+    lseek(fd, 8, SEEK_END);
+    read(fd, result, sizeof(char) * 8);
+    return result;
 }
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int result[2];
 
-    if (argc < 2) return -1;
-    read_end(argv[1], (char *) result);
+    if (argc < 2)
+        return -1;
+    read_end(argv[1], (char *)result);
     printf("magic number: %d\n", (result[0] ^ result[1]) % 1000);
     return 0;
 }
