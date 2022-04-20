b2227b6dba4f4a0d45b12bcdbf0ec562f7a3cf2f
MatthewCga <mateusz.cga@gmail.com>
2022-04-20 09:58:16 +0200

    siwik 3

--- OLD_kol1/Siwik/zad2/main.c
+++ OLD_kol1/Siwik/zad2/main.c
@@ -6,44 +6,48 @@
 
 int main(int argc, char *argv[])
 {
-   if (argc != 2)
-   {
-      printf("Not a suitable number of program parameters\n");
-      exit(1);
-   }
+    if (argc != 2)
+    {
+        printf("Not a suitable number of program parameters\n");
+        exit(1);
+    }
 
-   int toChildFD[2], toParentFD[2];
+    int toChildFD[2], toParentFD[2];
 
-   pipe(toChildFD);
-   pipe(toParentFD);
+    pipe(toChildFD);
+    pipe(toParentFD);
 
-   int val1, val2, val3 = 0;
+    int val1, val2, val3 = 0;
 
-   pid_t pid;
+    pid_t pid;
 
-   if ((pid = fork()) == 0)
-   {
-      close(toChildFD[1]);
-      read(toChildFD[0], &val2, sizeof(int));
+    // *********************************************************
+    // rodzic ma przekazac dziecku wartosc i ma otrzymac jej kwadrat
+    // *********************************************************
 
-      val2 = val2 * val2;
+    if ((pid = fork()) == 0)
+    {
+        close(toChildFD[1]);
+        read(toChildFD[0], &val2, sizeof(int));
 
-      close(toParentFD[0]);
-      write(toParentFD[1], &val2, sizeof(int));
-   }
-   else
-   {
-      val1 = atoi(argv[1]);
+        val2 = val2 * val2;
 
-      close(toChildFD[0]);
-      write(toChildFD[1], &val1, sizeof(int));
+        close(toParentFD[0]);
+        write(toParentFD[1], &val2, sizeof(int));
+    }
+    else
+    {
+        val1 = atoi(argv[1]);
 
-      sleep(1);
+        close(toChildFD[0]);
+        write(toChildFD[1], &val1, sizeof(int));
 
-      close(toParentFD[1]);
-      read(toParentFD[0], &val3, sizeof(int));
+        sleep(1);
 
-      printf("%d square is: %d\n", val1, val3);
-   }
-   return 0;
+        close(toParentFD[1]);
+        read(toParentFD[0], &val3, sizeof(int));
+
+        printf("%d square is: %d\n", val1, val3);
+    }
+    return 0;
 }
--- OLD_kol1/Siwik/zad3/main.c
+++ OLD_kol1/Siwik/zad3/main.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
     int i, pid;
 
-    if(argc != 2){
+    if (argc != 2)
+    {
         printf("Not a suitable number of program arguments");
         exit(2);
-    }else {
-       for (i = 0; i < atoi(argv[1]); i++) {
-        //*********************************************************
-        //Uzupelnij petle w taki sposob aby stworzyc dokladnie argv[1] procesow potomnych, bedacych dziecmi
-        //   tego samego procesu macierzystego.
-           // Kazdy proces potomny powinien:
-               // - "powiedziec ktorym jest dzieckiem",
-                //-  jaki ma pid,
-                //- kto jest jego rodzicem
-           //******************************************************
+    }
+    else
+    {
+        for (i = 0; i < atoi(argv[1]); i++)
+        {
+            //*********************************************************
+            // Uzupelnij petle w taki sposob aby stworzyc dokladnie
+            // argv[1] procesow potomnych, bedacych dziecmi
+            // tego samego procesu macierzystego.
+            // Kazdy proces potomny powinien:
+            // - powiedziec ktorym jest dzieckiem
+            // - jaki ma pid
+            // - kto jest jego rodzicem
+            //******************************************************
+
+            if ((pid = fork()) == 0)
+            {
+                printf("child: %d\tpid: %d\tparent pid: %d\n", i + 1, getpid(), getppid());
+                exit(0);
+            }
         }
     }
     return 0;
